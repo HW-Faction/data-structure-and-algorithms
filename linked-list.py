af667b0a4c50574a36607320c63d7f1b85c01eb4
HW-Faction <hankwilliamson49@gmail.com>
2019-09-29 13:36:37 +0530

    add append method

---
+++ linked-list.py
@@ -0,0 +1,22 @@
+# LinkedList in Python by HW & PSN
+import logging
+
+
+class Elements(object):  # Element Class
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList(object):  # LinkedList Class
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
